beab8c45388a5d6324cd46c0a23ebf4c2c95dfde
fairyhunter13 <fairyhunter13@gmail.com>
2020-06-18 13:40:41 +0700

    - Finishing consumer consume features :sparkles: - Finishing producer publish features :sparkles: - Adding consumer args :sparkles:

--- consumer.go
+++ consumer.go
@@ -1,13 +1,70 @@
 package kafkaclient
 
-import "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/panjf2000/ants/v2"
+)
 
 // Consumer promotes the origin consumer of kafka client.
 type Consumer struct {
 	*kafka.Consumer
 }
 
+// EventHandler is a handler for handling event.
+type EventHandler func(cons *Consumer, event kafka.Event)
+
+// MessageHandler is a handler for handling message.
+type MessageHandler func(cons *Consumer, msg *kafka.Message, err error)
+
 // GetOrigin returns the origin consumer of kafka.
 func (c *Consumer) GetOrigin() *kafka.Consumer {
 	return c.Consumer
+}
+
+// Consume consumes the messages from the kafka broker using MessageHandler.
+func (c *Consumer) Consume(args ConsumeArgs) (err error) {
+	args.fixArgs()
+	err = c.SubscribeTopics(args.Topics, args.RebalanceCb)
+	if err != nil {
+		return
+	}
+
+	for numWorker := uint64(1); numWorker <= args.Workers; numWorker++ {
+		ants.Submit(func() {
+			var (
+				err error
+				msg *kafka.Message
+			)
+			for {
+				msg, err = c.ReadMessage(time.Duration(args.Polling) * time.Millisecond)
+				args.Handler(c, msg, err)
+			}
+		})
+	}
+
+	return
+}
+
+// ConsumeEvent consumes the events from the kafka broker using EventHandler.
+func (c *Consumer) ConsumeEvent(args ConsumeArgs) (err error) {
+	args.fixArgs()
+	err = c.SubscribeTopics(args.Topics, args.RebalanceCb)
+	if err != nil {
+		return
+	}
+
+	for numWorker := uint64(1); numWorker <= args.Workers; numWorker++ {
+		ants.Submit(func() {
+			var (
+				event kafka.Event
+			)
+			for {
+				event = c.Poll(args.Polling)
+				args.EventHandler(c, event)
+			}
+		})
+	}
+	return
 }
--- producer.go
+++ producer.go
@@ -1,6 +1,8 @@
 package kafkaclient
 
-import "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 // Producer promotes the producer of kafka client.
 type Producer struct {
@@ -15,5 +17,23 @@ func (p *Producer) GetOrigin() *kafka.Producer {
 // Close close the underlying resources.
 func (p *Producer) Close() (err error) {
 	p.Producer.Close()
+	return
+}
+
+// Publish publishes message synchronously to the kafka's brokers.
+func (p *Producer) Publish(msg *kafka.Message) (event kafka.Event, err error) {
+	eventChan := make(chan kafka.Event, 1)
+	defer close(eventChan)
+	err = p.Produce(msg, eventChan)
+	if err != nil {
+		return
+	}
+	event = <-eventChan
+	return
+}
+
+// PublishAsync publishes message asynchronously to the kafka's brokers.
+func (p *Producer) PublishAsync(msg *kafka.Message, eventChan chan kafka.Event) (err error) {
+	err = p.Produce(msg, eventChan)
 	return
 }
